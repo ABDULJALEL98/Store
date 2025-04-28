eeda0b89657582d73ed30174895dc70f34211496
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2025-04-28 17:36:10 +0300

    Some changes

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -2,6 +2,7 @@ using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.Contracts;
+using StoreApp.Infrastructure.Extensions;
 
 namespace StoreApp.Pages
 {
@@ -10,16 +11,17 @@ namespace StoreApp.Pages
       private readonly IServiceManager _manager;
        public Cart Cart { get; set; } //IoC
       public string ReturnUrl { get; set; } ="/";
-        public CartModel(IServiceManager manager, Cart cart)
+        public CartModel(IServiceManager manager)
         {
             _manager = manager;
-            Cart = cart;
+           
         }
         
 
       public void OnGet(string returnUrl)
       {
             ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
           
       }
        
@@ -31,7 +33,9 @@ namespace StoreApp.Pages
 
         if (product is not null)
         {
+          Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product,1);
+            HttpContext.Session.SetJson<Cart>("cart", Cart);
         }
         return Page(); //returnUrl
        }
@@ -39,7 +43,9 @@ namespace StoreApp.Pages
        public IActionResult OnPostRemove(int id, string returnUrl)
 
         {
+          Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
           Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+          HttpContext.Session.SetJson<Cart>("cart", Cart);
           return Page(); 
         }
 
