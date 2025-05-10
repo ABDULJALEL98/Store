160ff3bcf14696165f41046d363d748aca67df1e
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2025-05-10 15:58:47 +0300

    WebApi

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -2,7 +2,7 @@ using StoreApp.Infrastructure.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -36,8 +36,8 @@ app.MapAreaControllerRoute(
 );
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-
 app.MapRazorPages();
+app.MapControllers();
 app.ConfigureAndCheckMigration();
 app.ConfigureLocalization();
 app.ConfigureDefaultAdminUser();
