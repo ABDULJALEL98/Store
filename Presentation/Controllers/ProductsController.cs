896f9e16c20712e967e82c0e5dc07519744dfbde
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2025-05-11 11:43:16 +0300

    Presentation_Layer

--- Presentation/Controllers/ProductsController.cs
+++ Presentation/Controllers/ProductsController.cs
@@ -1,17 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
 
-namespace StoreApp.Api
+namespace Presentation.Controllers
 {
+    
     [Route("api/products")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
         private readonly IServiceManager _manager;
+
         public ProductsController(IServiceManager manager)
         {
             _manager = manager;
         }
+        
         [HttpGet]
         public IActionResult GetAllProducts()
         {
